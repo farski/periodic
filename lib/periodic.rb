c266d45abf6130d8726bb3f45394b2613bfdbc6c
Chris Kalafarski <chris@farski.com>
2009-02-25 13:52:04 -0500

    0.1 library

---
+++ lib/periodic.rb
@@ -0,0 +1,77 @@
+module Periodic
+	extend self
+	
+	def parse(string, options = {})
+		return nil if !string.match(/\d/)
+		options[:bias] ||= :seconds
+		seconds = expand(delimit(string, options))
+		return (options[:seconds] == :partial && !seconds.is_a?(Float)) ? seconds.to_f : seconds
+	end
+	
+	def output(seconds, format, precision = :round)
+		return nil unless (seconds.is_a?(Integer) || seconds.is_a?(Float)) && format.match(/%/)
+		precision = (precision == :exact ? 'to_f' : 'to_i')
+		
+		names = %w{ seconds minutes hours days weeks months years }
+		factors = [1, 60, 60, 24, 7, 30, 365]
+		matchers = [/%s/, /%m/, /%h/, /%d/, /%w/, /%n/, /%y/]
+		
+		units = matchers.inject(Array.new) { |cache, m| cache << (format.match(m) ? names[matchers.index(m)] : nil) }.compact
+		units.reverse.each_with_index do |u, i|
+			factor = eval(factors[0,names.index(u)+1].join("*"))
+			value = (seconds.send(units[i+1] ? 'to_i' : precision) / factor)
+			seconds -= (value*factor)
+			value = ((format.match(/:/) && value.to_s.length == 1) ? ("0" + value.to_s) : value.to_s) 
+			format.gsub!(matchers[names.index(u)], value)
+		end
+		return sanitize(format).strip
+	end
+	
+private
+
+	def delimit(string, options)
+		units = [:seconds, :minutes, :hours, :days, :weeks, :months, :years, :decades, :centuries, :millennia]
+		abbrs = %w{s m h d w n y a c b}
+		if string.match(/[a-zA-Z]/)
+			string.concat("sec") if string[-1,1].match(/\d/)			
+			parts = normalize_text(string).split(' ').sort{|x,y| abbrs.index(y[-1,1]) <=> abbrs.index(x[-1,1]) }				
+			string = ""
+			parts.each_with_index do |s, i|
+				string << (s.to_i == s.to_f ? s.to_i : s.to_f).to_s + ":"
+				if parts[i+1]
+					((abbrs.index(s[-1,1]) - abbrs.index(parts[i+1][-1,1]))-1).times{ string << '0:' }
+				elsif s[-1,1] != "s"
+					(abbrs.index(s[-1,1])).times{ string << '0:' }
+				end
+			end
+			string.chop!
+		end
+		minimum_bias = string.match(/:/) ? string.scan(/:/).size : 0			
+		options[:bias] = units[minimum_bias] if units.index(options[:bias]) < minimum_bias
+		units.reverse.index(options[:bias]).times{ string.insert(0, '00:') }			
+		(units.index(options[:bias])-minimum_bias).times{ string.concat(':00') }
+		return string
+	end
+	
+	def normalize_text(string)
+		[/(and |,)/, /( )/].each{ |m| string.gsub!(m, '') }
+		string.gsub!(/(\d)([a-zA-Z]+)/, '\1\2 ')
+		{:s=>/(s\w*)/,:n=>/(mo\w*)/,:b=>/(m\w*l\w*)/,:m=>/(m\w*)/,:h=>/(h\w*)/,:a=>/(d\w*c\w*)/,:d=>/(d\w*)/,:w=>/(w\w*)/,:y=>/(y\w*)/,:c=>/(c\w*)/}.each{ |k,v| string.gsub!(v, k.to_s) }
+		string
+	end
+		
+	def expand(string)
+		factors, seconds = [1, 60, 3600, (3600*24), (3600*24*7), (3600*24*30), (3600*24*365.25), (3600*24*365.25*10), (3600*24*365.25*100), (3600*24*365.25*1000)], 0
+		string.split(/:/).each_with_index { |n, i| seconds += n.to_f * factors.reverse[i] }
+		return (seconds == seconds.to_i ? seconds.to_i : seconds)
+	end
+
+	def sanitize(string)
+		if string.match(/:/)
+			string.gsub!(/^([0:]*)/, '').gsub!(/:(\d[^\d])/, ':0\1')
+		else
+			string.gsub!(/^(0[a-zA-Z, ]+)*/, '').gsub!(/([a-zA-Z, ]+)(0[^!]([a-zA-Z ]+)*)*([1-9]+)*/, '\1\4')
+		end
+		string.gsub!(/!*/, '')
+	end
+end
--- test/periodic_test.rb
+++ test/periodic_test.rb
@@ -1,7 +1,140 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class PeriodicTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+	context "The parser" do
+		should "return nil if the string can't be parsed" do
+			assert_nil Periodic.parse('not a valid duration')
+		end
+		
+		should "return an integer if there are no partial seconds" do
+			assert_kind_of Integer, Periodic.parse('1 second')
+			assert_kind_of Integer, Periodic.parse('1 minute')
+			assert_kind_of Integer, Periodic.parse('1.0 minute')
+			assert_kind_of Integer, Periodic.parse('1.25 minutes')
+			assert_kind_of Integer, Periodic.parse('1 minute 1 second')
+			assert_kind_of Integer, Periodic.parse('1.25 minutes 1 second')
+		end
+		
+		should "return a float is there are partial seconds" do
+			assert_kind_of Float, Periodic.parse('.1 second')
+			assert_kind_of Float, Periodic.parse('0.1 second')
+			assert_kind_of Float, Periodic.parse('1.01 minutes')
+			assert_kind_of Float, Periodic.parse('1 minute 0.1 second')
+		end
+		
+		should "parse solitary numbers into seconds without labels" do
+			assert_equal 1, Periodic.parse('1')
+			assert_equal 1, Periodic.parse('1.0')
+			assert !1.eql?(Periodic.parse('1.0', :seconds => :partial))
+			assert 1.0.eql?(Periodic.parse('1.0', :seconds => :partial))
+		end
+		
+		should "parse solitary numbers appropriately with a bias" do
+			assert_equal 60, Periodic.parse('1', :bias => :minutes)
+			assert_equal 3600, Periodic.parse('1', :bias => :hours)
+			assert_equal((3600*24*365.25), Periodic.parse('1', :bias => :years))
+			assert_equal((3600*24*365.25*100), Periodic.parse('1', :bias => :centuries))
+		end
+		
+		context "with text labels" do
+			context "with a single unit" do
+				should "parse seconds with a variety of labels" do
+					%q{s sec secs second seconds}.each do |l|
+						assert_equal(1, Periodic.parse('1' + l))
+						assert_equal(1, Periodic.parse('1 ' + l))
+					end
+					assert !1.eql?(Periodic.parse('1.0 second', :seconds => :partial))
+					assert 1.0.eql?(Periodic.parse('1.0 second', :seconds => :partial))
+				end
+		
+				should "parse minutes with a variety of labels" do
+					%q{m min mins minute minutes}.each do |l|
+						assert_equal(60, Periodic.parse('1' + l))
+						assert_equal(60, Periodic.parse('1 ' + l))
+					end
+				end
+		
+				should "parse hours with a variety of labels" do
+					%q{h hs hr hrs hour hours}.each do |l|
+						assert_equal(3600, Periodic.parse('1' + l))
+						assert_equal(3600, Periodic.parse('1 ' + l))
+					end
+				end
+			end
+			
+			context "with several units" do
+				should "parse with a variety of labels and separators" do
+					assert_equal 61, Periodic.parse('1m1s')
+					assert_equal 61, Periodic.parse('1 minute 1 second')
+					assert_equal 61, Periodic.parse('1 minute and 1 second')
+					assert_equal 61, Periodic.parse('1 minute, 1 second')
+					assert_equal 3661, Periodic.parse('1 hour, 1 minute and 1 second')
+					assert_equal 3661.1, Periodic.parse('1 hour, 1 minute and 1.1 seconds')
+				end
+				
+				should "parse even in unusual situations" do
+					assert_equal 120, Periodic.parse('01 minute 60 seconds')
+					assert_equal 120, Periodic.parse('1.5 minutes 30 seconds')
+					assert_equal 90.1, Periodic.parse('1.5 minutes .1 seconds')
+				end
+			end
+		end
+		
+		context "with symbol delimiters" do
+			should "parse basic formats" do
+				assert_equal 90, Periodic.parse('1:30')
+				assert_equal 90.1, Periodic.parse('1:30.1')
+				assert_equal 60, Periodic.parse('1:00')
+			end
+			
+			should "parse with a bias" do
+				assert_equal 5400, Periodic.parse('1:30', :bias => :hours)
+			end
+			
+			should "parse with a bias but also correct any impossibilities" do
+				assert_equal 90, Periodic.parse('1:30', :bias => :seconds) # will correct to :bias => minute
+			end
+		end
+	end
+
+	context "The printer" do
+		should "return nil if the arguments aren't correct" do
+			assert_nil Periodic.output('foo', '%s')
+			assert_nil Periodic.output(123, 'foo')
+		end
+		
+		should "return the exact number of seconds input" do
+			assert_equal "1234", Periodic.output(1234, "%s")
+		end
+		
+		should "print simple 'conversions'" do
+			assert_equal "1", Periodic.output((60*60), "%h")
+			assert_equal "1", Periodic.output((60*60*24), "%d")
+			assert_equal "1", Periodic.output((60*60*24*7), "%w")
+		end
+		
+		should "print text without extraneous bits" do
+			assert_equal "1h", Periodic.output((60*60), "%hh %ss")
+		end
+		
+		should "print text with forced extraneous bits" do
+			assert_equal "1h 0s", Periodic.output((60*60), "%hh %s!s")
+		end
+		
+		should "print digital without extraneous bits" do
+			assert_equal "1:00:00", Periodic.output((60*60), "%d:%h:%m:%s")
+		end
+		
+		should "print digital with forced extraneous bits" do
+			assert_equal "00:01:00:00", Periodic.output((60*60), "!%d!:%h:%m:%s")
+		end
+	end
+
+	context "The program" do
+		should "maintain integrity throughout parsing and printing" do
+			assert_equal 120, Periodic.parse(Periodic.output(Periodic.parse('2 minutes'), '%s'))
+			assert_equal "120", Periodic.output(Periodic.parse(Periodic.output(120, '%s')), '%s')
+			assert_equal "24:00:01", Periodic.output(Periodic.parse(Periodic.output(86401, '%h:%m:%s')), '%h:%m:%s')
+		end
+	end
 end
